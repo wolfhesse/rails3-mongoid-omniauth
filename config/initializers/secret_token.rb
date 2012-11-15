e231bedb4632c7f7788dcc8cdd966b93d05c7cb2
wolfhesse <wolfgang.schuessel@github.com>
2012-11-15 16:26:55 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3MongoidOmniauth::Application.config.secret_token = '3708e6b0410b498cc2c2443fddba24a240ecd1b99850719d6b4bbd527b2565f4c17e4cb7395e20114dda23728b384536f15116c476f60a02ce77b1ce937e0406'
